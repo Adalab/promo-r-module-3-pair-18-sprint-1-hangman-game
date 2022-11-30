3722050e936b9e672497da0354dc79b0dca7896e
Tatiana Arango Cardona <tatianarangoc@gmail.com>
2022-11-30 10:13:51 +0100

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 // Fichero src/components/App.js
 import '../styles/App.scss';
 import { useEffect, useState } from 'react';
-import getWords from '../../src/services/api'
+import getWords from '../../src/services/api';
+import Header from './Hader';
+import Dummy from './Dummy';
 // import React from 'react';
 
 function App() {
@@ -10,13 +12,12 @@ function App() {
   const [word, setWord] = useState('');
   const [userLetters, setUserLetters] = useState([]);
 
-
-  useEffect (() => {
-    getWords().then (data => {
+  useEffect(() => {
+    getWords().then((data) => {
       setWord(data.word);
-      console.log(data.word)
+      console.log(data.word);
     });
-  },[]);
+  }, []);
   // const handleClick = () => {
   //   setNumber(numbeOfrErrors + 1);
   // };
@@ -28,22 +29,22 @@ function App() {
       if (event.target.value !== '') {
         setUserLetters([...userLetters, event.target.value]);
         userLetters.map((eachLetter, index) => {
-      if (wordLetters.includes(eachLetter)) {
-        // x
-      } else { 
-        setNumber(numbeOfrErrors + 1);
-        // aqui lo dificil. primer if le digo que si la letra que mete la usuaria
-        // cumple requisitos o si esta vacia, y si los cumple me la quedo
-        //con esa letra que me he quedado valoro si es diferente a nada y si lo cumple la meto en
-        // userlleter. Y ademas de meterla en userleter, le hago un mapeo a userleter
-        // y le pregunto que si la letrita que mete la usuaria
-        // esta por cada letrita de worsleter( palabra traida por fetch)
-        // esta incluida . Si SI esta incluida no hago nada por que ya lo estoy haciendo en otra funcion
-        //y si no esta incluida meto la funcion que teniamos desde el principio para añadir el error. y que lo pinte
-      };
-        })
-        }
+          if (wordLetters.includes(eachLetter)) {
+            // x
+          } else {
+            setNumber(numbeOfrErrors + 1);
+            // aqui lo dificil. primer if le digo que si la letra que mete la usuaria
+            // cumple requisitos o si esta vacia, y si los cumple me la quedo
+            //con esa letra que me he quedado valoro si es diferente a nada y si lo cumple la meto en
+            // userlleter. Y ademas de meterla en userleter, le hago un mapeo a userleter
+            // y le pregunto que si la letrita que mete la usuaria
+            // esta por cada letrita de worsleter( palabra traida por fetch)
+            // esta incluida . Si SI esta incluida no hago nada por que ya lo estoy haciendo en otra funcion
+            //y si no esta incluida meto la funcion que teniamos desde el principio para añadir el error. y que lo pinte
+          }
+        });
       }
+    }
   };
 
   const renderSolutionLetters = () => {
@@ -56,21 +57,15 @@ function App() {
           </li>
         );
       } else {
-        
         return <li key={index} className="letter"></li>;
-        
-
       }
     });
   };
   const renderErrorLetters = () => {
-   
-    
     const wordLetters = word.split('');
     return userLetters.map((eachLetter, index) => {
       if (wordLetters.includes(eachLetter)) {
-    
-      } else { 
+      } else {
         return (
           <li key={index} className="letter">
             {eachLetter}
@@ -82,7 +77,7 @@ function App() {
   return (
     <div className="page">
       <header>
-        <h1 className="header__title">Juego del ahorcado</h1>
+        <Header />
       </header>
       <main className="main">
         <section>
@@ -113,24 +108,12 @@ function App() {
           {/* <button onClick={handleClick}>Incrementar</button> */}
         </section>
 
-        <section className={`dummy error-${numbeOfrErrors}`}>
-          <span className="error-13 eye"></span>
-          <span className="error-12 eye"></span>
-          <span className="error-11 line"></span>
-          <span className="error-10 line"></span>
-          <span className="error-9 line"></span>
-          <span className="error-8 line"></span>
-          <span className="error-7 line"></span>
-          <span className="error-6 head"></span>
-          <span className="error-5 line"></span>
-          <span className="error-4 line"></span>
-          <span className="error-3 line"></span>
-          <span className="error-2 line"></span>
-          <span className="error-1 line"></span>
+        <section>
+          <Dummy />
         </section>
       </main>
     </div>
   );
-  }
+}
 
 export default App;
